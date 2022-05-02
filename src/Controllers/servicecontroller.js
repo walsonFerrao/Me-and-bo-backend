166ec5acc7e424ba4b049e62026b36a9c4ab0028
walson <walsongodwin@gmail.com>
2022-05-02 22:28:53 +0530

    added get by id route

--- src/Controllers/servicecontroller.js
+++ src/Controllers/servicecontroller.js
@@ -71,6 +71,54 @@ router.get("/:id",async (req,res)=>{
     
     })
 
+    router.get("/user/:id",async (req,res)=>{
+
+        try{
+        
+        const service=await Service.find({user:req.params.id}).lean().exec()
+        
+        res.status(201).send(service)
+        
+        
+        
+        }
+        catch(err)
+        {
+        
+        console.log(err)
+        res.status(500).send(err)
+        
+        
+        }
+        
+        
+        })
+
+
+        router.delete("/user/:id",async (req,res)=>{
+
+            try{
+            
+            const service=await Service.findByIdAndDelete(req.params.id)
+            
+            res.status(201).send(service)
+            
+            
+            
+            }
+            catch(err)
+            {
+            
+            console.log(err)
+            res.status(500).send(err)
+            
+            
+            }
+            
+            
+            })
+    
+
 
 
 module.exports=router
